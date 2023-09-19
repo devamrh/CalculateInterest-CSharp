2c09ece3611a26e13b1d026faac137bd68603fa3
A. M. Rafinul Huq <116501394+devamrh@users.noreply.github.com>
2023-09-19 22:07:02 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace CalculateInterest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Money Input
+            Console.WriteLine("Enter the money amount (In Taka):");
+
+            double moneyAmount = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(moneyAmount);
+
+            // Interest Input
+            Console.WriteLine("Enter the interest amount (In %):");
+
+            double InterestAmount = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(InterestAmount);
+
+            // Period Input
+            Console.WriteLine("Enter the period (In Year):");
+
+            double yearAmount = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine(yearAmount);
+
+
+            double simpleInterest = ( moneyAmount * InterestAmount * yearAmount )/ 100;
+
+            Console.WriteLine($"The Simple Interest : {simpleInterest}");
+
+
+
+
+
+
+        }
+    }
+}
